084d825e35ebca205a74249ea19179050caae8a9
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2024-11-21 10:13:32 +1100

    added files for servo.h and ultrasonic.h .... done basic code on neuro.grip

--- Neuro.Grip/Neuro.Grip.ino
+++ Neuro.Grip/Neuro.Grip.ino
@@ -1,3 +1,4 @@
+/*
 Vladimir:
 https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Servo_Motor
 Servo 1 pin 3
@@ -6,4 +7,31 @@ Servo 3 pin 5
 
 Zaif:
 https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Ultrasonic_Ranger
-US sensor pin 1
+US sensor pin 7
+*/
+
+#include <Servo.h>
+#include "Ultrasonic.h"
+
+Ultrasonic distanceSensor(7);
+
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+
+int pos = 0;
+
+void setup()
+{
+  myservo1.attach(3);
+  myservo2.attach(4);
+  myservo3.attach(5);
+}
+
+void loop()
+{
+
+}
+
+
+
