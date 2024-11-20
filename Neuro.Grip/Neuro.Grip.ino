3a040933697020bbfaefbaae41f4430e2c12fbd2
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2024-11-20 12:11:12 +1100

    Added servos

---
+++ Neuro.Grip/Neuro.Grip.ino
@@ -0,0 +1,9 @@
+Vladimir:
+https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Servo_Motor
+Servo 1 pin 3
+Servo 2 pin 4
+Servo 3 pin 5
+
+Zaif:
+https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue/Sensor_Kit/Ultrasonic_Ranger
+US sensor pin 1
