f403e1c50bcd8a5d93fccfbd1606646f19c0fb68
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2024-11-21 09:59:14 +1100

    3 servo motors working

--- Vladimir.G/Vladimir.G.ino
+++ Vladimir.G/Vladimir.G.ino
@@ -1,16 +1,20 @@
-Author: Vladimir-G 20/11/24 - ../12/24
+//Author: Vladimir-G 20/11/24 - ../12/24
 
 
 
 #include <Servo.h> // library
 
-Servo myservo; // asign name
+Servo myservo1; // asign name
+Servo myservo2;
+Servo myservo3;
 
 int pos = 0; // starting position
 
 void setup() 
 {
-  myservo.attach(5); // asign pins
+  myservo1.attach(3);
+  myservo2.attach(4);
+  myservo3.attach(5); // asign pins
 }
 
 //change void loop to be measured when to move servo motors by read measurement of ultrasonic sensor
@@ -18,18 +22,21 @@ void loop()
 {
   for (pos = 0; pos <= 180; pos += 1) // change in position
   { 
-    myservo.write(pos);           
+    myservo1.write(pos);
+    myservo2.write(pos);
+    myservo3.write(pos);
+
     delay(15); // wait 15 milliseconds            
   }
   for (pos = 180; pos >= 0; pos -= 1) // change in position with math
   {
-    myservo.write(pos);
+    myservo1.write(pos);
+    myservo2.write(pos);
+    myservo3.write(pos);
     delay(15); // wait 15 milliseconds       
   }
 }
 
-
-//custom viod loop
 
 /*
 
