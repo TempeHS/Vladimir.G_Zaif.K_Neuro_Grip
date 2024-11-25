137f8211244a4b516ff34991060afcfef46f45ce
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2024-11-25 14:08:48 +1100

    Changed to old code servo motors are not working

--- Vladimir.G/Vladimir.G.ino
+++ Vladimir.G/Vladimir.G.ino
@@ -1,7 +1,5 @@
 //Author: Vladimir-G 20/11/24 - ../12/24
 
-
-
 #include <Servo.h> // library
 
 Servo myservo1; // asign name
@@ -18,27 +16,24 @@ void setup()
 }
 
 //change void loop to be measured when to move servo motors by read measurement of ultrasonic sensor
-void loop() 
+
+void loop()
 {
   for (pos = 0; pos <= 180; pos += 1) // change in position
-  { 
+  {           
     myservo1.write(pos);
     myservo2.write(pos);
     myservo3.write(pos);
 
     delay(15); // wait 15 milliseconds            
   }
-  for (pos = 180; pos >= 0; pos -= 1) // change in position
+  for (pos = 180; pos >= 0; pos -= 1) // change in position with math
   {
     myservo1.write(pos);
     myservo2.write(pos);
     myservo3.write(pos);
-    
     delay(15); // wait 15 milliseconds       
   }
 }
 
 
-/*
-
-*/
