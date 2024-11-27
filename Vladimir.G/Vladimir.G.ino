6be4c09889bd719c53f609a80f68ecb6144e1963
Vladimir G <159206796+Vladimir-Grebenets@users.noreply.github.com>
2024-11-27 14:54:01 +1100

    added function.ino

--- Vladimir.G/Vladimir.G.ino
+++ Vladimir.G/Vladimir.G.ino
@@ -10,30 +10,14 @@ int pos = 0; // starting position
 
 void setup() 
 {
-  myservo1.attach(3);
-  myservo2.attach(4);
-  myservo3.attach(5); // asign pins
+  servosetup();
 }
 
 //change void loop to be measured when to move servo motors by read measurement of ultrasonic sensor
 
 void loop()
 {
-  for (pos = 0; pos <= 180; pos += 1) // change in position
-  {           
-    myservo1.write(pos);
-    myservo2.write(pos);
-    myservo3.write(pos);
-
-    delay(15); // wait 15 milliseconds            
-  }
-  for (pos = 180; pos >= 0; pos -= 1) // change in position with math
-  {
-    myservo1.write(pos);
-    myservo2.write(pos);
-    myservo3.write(pos);
-    delay(15); // wait 15 milliseconds       
-  }
+  servorun();
 }
 
 
---
+++ Vladimir.G/vladimir.g_functions.ino
@@ -0,0 +1,23 @@
+void servosetup() {
+  myservo1.attach(3);
+  myservo2.attach(4);
+  myservo3.attach(5); // asign pins
+}
+
+void servorun() {
+  for (pos = 0; pos <= 180; pos += 1) // change in position
+  {           
+    myservo1.write(pos);
+    myservo2.write(pos);
+    myservo3.write(pos);
+
+    delay(15); // wait 15 milliseconds            
+  }
+  for (pos = 180; pos >= 0; pos -= 1) // change in position with math
+  {
+    myservo1.write(pos);
+    myservo2.write(pos);
+    myservo3.write(pos);
+    delay(15); // wait 15 milliseconds       
+  }
+}
